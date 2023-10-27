ad3c72c538efb64ed222420427c5e6dc99d5c8b0
dogadeniz <dogadeniz08@hotmail.com>
2023-10-27 11:18:26 +0300

    FormRegistProject

---
+++ WindowsForm-StudentRegisterationSystem/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace lab3
+{
+	partial class Form1
+	{
+		/// <summary>
+		///Gerekli tasarımcı değişkeni.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///Kullanılan tüm kaynakları temizleyin.
+		/// </summary>
+		///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer üretilen kod
+
+		/// <summary>
+		/// Tasarımcı desteği için gerekli metot - bu metodun 
+		///içeriğini kod düzenleyici ile değiştirmeyin.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.StudentNo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.StudentName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Age = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Registered = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label1.Location = new System.Drawing.Point(93, 63);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(125, 25);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "StudentNo :";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label2.Location = new System.Drawing.Point(93, 107);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(80, 25);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Name :";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label3.Location = new System.Drawing.Point(93, 156);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(62, 25);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Age :";
+			this.label3.Click += new System.EventHandler(this.label3_Click);
+			// 
+			// listView1
+			// 
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.StudentNo,
+            this.StudentName,
+            this.Age,
+            this.Registered});
+			this.listView1.HideSelection = false;
+			this.listView1.Location = new System.Drawing.Point(72, 265);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(317, 127);
+			this.listView1.TabIndex = 3;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			// 
+			// StudentNo
+			// 
+			this.StudentNo.Text = "Number";
+			// 
+			// StudentName
+			// 
+			this.StudentName.Text = "Name";
+			this.StudentName.Width = 61;
+			// 
+			// Age
+			// 
+			this.Age.Text = "Age";
+			// 
+			// Registered
+			// 
+			this.Registered.Text = "Registered";
+			this.Registered.Width = 86;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(230, 63);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 4;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(230, 113);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 5;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(230, 156);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 6;
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button2.Location = new System.Drawing.Point(161, 204);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(111, 41);
+			this.button2.TabIndex = 8;
+			this.button2.Text = "New";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button1.Location = new System.Drawing.Point(44, 204);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(111, 41);
+			this.button1.TabIndex = 9;
+			this.button1.Text = "Insert";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button4
+			// 
+			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button4.Location = new System.Drawing.Point(278, 204);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(111, 41);
+			this.button4.TabIndex = 11;
+			this.button4.Text = "Remove";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+			this.comboBox1.Location = new System.Drawing.Point(518, 62);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 12;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.checkBox1.Location = new System.Drawing.Point(459, 113);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(120, 28);
+			this.checkBox1.TabIndex = 15;
+			this.checkBox1.Text = "Registered";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label4.Location = new System.Drawing.Point(387, 62);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(95, 25);
+			this.label4.TabIndex = 16;
+			this.label4.Text = "Gender :";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ColumnHeader StudentNo;
+		private System.Windows.Forms.ColumnHeader StudentName;
+		private System.Windows.Forms.ColumnHeader Age;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ColumnHeader Registered;
+	}
+}
+
---
+++ WindowsForm-StudentRegisterationSystem/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+			ListViewItem newItem;
+			if (comboBox1.Text == "Male")
+			{
+				newItem = new ListViewItem(textBox1.Text + " {M}");
+			}
+			else if (comboBox1.Text == "Female")
+			{
+				newItem = new ListViewItem(textBox1.Text + " {F}");
+			}
+			else
+			{
+				newItem = new ListViewItem(textBox1.Text);
+			}
+
+			newItem.SubItems.Add(textBox2.Text);
+			newItem.SubItems.Add(textBox3.Text);
+			if (checkBox1.Checked)
+			{
+				newItem.SubItems.Add(checkBox1.Text);
+			}
+			else
+			{
+				newItem.SubItems.Add("NotRegistered");
+			}
+			listView1.Items.Add(newItem);
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = "";
+			textBox2.Text = "";
+			textBox3.Text = "";
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			if(listView1.SelectedItems.Count > 0)
+			{
+				listView1.Items.Remove(listView1.SelectedItems[0]);
+			}
+		}
+
+		
+		}
+	}
+
