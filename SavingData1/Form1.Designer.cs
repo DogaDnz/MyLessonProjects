159f28cb9aa9ac3d0751b1f7203217e964b9a266
dogadeniz <dogadeniz08@hotmail.com>
2023-11-04 16:22:47 +0300

    SavingData
    How to save and show a data with using c# form & SQL

---
+++ SavingData1/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace SavingData1
+{
+	partial class Form1
+	{
+		/// <summary>
+		///Gerekli tasarımcı değişkeni.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///Kullanılan tüm kaynakları temizleyin.
+		/// </summary>
+		///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer üretilen kod
+
+		/// <summary>
+		/// Tasarımcı desteği için gerekli metot - bu metodun 
+		///içeriğini kod düzenleyici ile değiştirmeyin.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.Names = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Company = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.PhoneNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.Gender = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.SuspendLayout();
+			// 
+			// listView1
+			// 
+			this.listView1.AllowDrop = true;
+			this.listView1.BackColor = System.Drawing.Color.Bisque;
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Names,
+            this.Company,
+            this.PhoneNumber,
+            this.Gender});
+			this.listView1.HideSelection = false;
+			this.listView1.Location = new System.Drawing.Point(381, 41);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(394, 145);
+			this.listView1.TabIndex = 0;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+			// 
+			// Names
+			// 
+			this.Names.Text = "Names";
+			this.Names.Width = 113;
+			// 
+			// Company
+			// 
+			this.Company.Text = "Company";
+			this.Company.Width = 105;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button1.Location = new System.Drawing.Point(162, 212);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(145, 40);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Show My Data";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button2.Location = new System.Drawing.Point(43, 212);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(113, 40);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Add";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label1.Location = new System.Drawing.Point(49, 41);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(172, 21);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Name  and Surname:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(226, 41);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 4;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(226, 81);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 6;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label2.Location = new System.Drawing.Point(49, 81);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(138, 21);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Company Name:";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(226, 120);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 8;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label3.Location = new System.Drawing.Point(49, 117);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(126, 21);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "PhoneNumber:";
+			// 
+			// PhoneNumber
+			// 
+			this.PhoneNumber.Text = "PhoneNumber";
+			this.PhoneNumber.Width = 109;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Female",
+            "Male"});
+			this.comboBox1.Location = new System.Drawing.Point(226, 154);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(100, 21);
+			this.comboBox1.TabIndex = 9;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label4.Location = new System.Drawing.Point(49, 154);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(70, 21);
+			this.label4.TabIndex = 10;
+			this.label4.Text = "Gender:";
+			// 
+			// Gender
+			// 
+			this.Gender.Text = "Gender";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ClientSize = new System.Drawing.Size(801, 418);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.listView1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ColumnHeader Names;
+		private System.Windows.Forms.ColumnHeader Company;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ColumnHeader PhoneNumber;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ColumnHeader Gender;
+	}
+}
+
---
+++ SavingData1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SavingData1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		SqlConnection con = new SqlConnection("Data Source=DESKTOP-0IHLFH6;Initial Catalog=Project2;Integrated Security=True");
+
+		private void ShowData() { 
+		con.Open();
+			SqlCommand com = new SqlCommand("Select *from Table1",con);
+			SqlDataReader red = com.ExecuteReader();
+
+			while (red.Read())
+			{
+				ListViewItem ad = new ListViewItem();
+
+				ad.Text = red["Names"].ToString();
+				ad.SubItems.Add(red["Company"].ToString());
+				ad.SubItems.Add(red["PhoneNumber"].ToString());
+				ad.SubItems.Add(red["Gender"].ToString());
+
+				listView1.Items.Add(ad);
+			}
+			con.Close();
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			ShowData();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			con.Open();
+			SqlCommand com = new SqlCommand("Insert Into Table1 (Names,Company,PhoneNumber,Gender) Values('" + textBox1.Text.ToString() + "' , '" + textBox2.Text.ToString() + "' , '" +textBox3.Text.ToString()+ "','" + comboBox1.Text.ToString() + "')", con);
+			com.ExecuteNonQuery();
+			con.Close();
+			textBox1.Clear();
+			textBox2.Clear();
+			textBox3.Clear();
+			comboBox1.Items.Clear();
+
+
+		}
+
+		private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
