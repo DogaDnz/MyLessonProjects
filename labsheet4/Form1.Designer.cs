d66ee8fa0a1ed96e285b8167eecc0cb29371c57a
dogadeniz <dogadeniz08@hotmail.com>
2023-11-08 18:11:58 +0300

    Mother-Child Project

---
+++ labsheet4/Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace labsheet4
+{
+	partial class Form1
+	{
+		/// <summary>
+		///Gerekli tasarımcı değişkeni.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///Kullanılan tüm kaynakları temizleyin.
+		/// </summary>
+		///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer üretilen kod
+
+		/// <summary>
+		/// Tasarımcı desteği için gerekli metot - bu metodun 
+		///içeriğini kod düzenleyici ile değiştirmeyin.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.age6 = new System.Windows.Forms.TextBox();
+			this.age5 = new System.Windows.Forms.TextBox();
+			this.age4 = new System.Windows.Forms.TextBox();
+			this.age3 = new System.Windows.Forms.TextBox();
+			this.age2 = new System.Windows.Forms.TextBox();
+			this.label12 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.mothername1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.search = new System.Windows.Forms.Button();
+			this.mothername2 = new System.Windows.Forms.TextBox();
+			this.label13 = new System.Windows.Forms.Label();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Controls.Add(this.age6);
+			this.groupBox1.Controls.Add(this.age5);
+			this.groupBox1.Controls.Add(this.age4);
+			this.groupBox1.Controls.Add(this.age3);
+			this.groupBox1.Controls.Add(this.age2);
+			this.groupBox1.Controls.Add(this.label12);
+			this.groupBox1.Controls.Add(this.label11);
+			this.groupBox1.Controls.Add(this.label10);
+			this.groupBox1.Controls.Add(this.label9);
+			this.groupBox1.Controls.Add(this.label8);
+			this.groupBox1.Controls.Add(this.textBox5);
+			this.groupBox1.Controls.Add(this.label6);
+			this.groupBox1.Controls.Add(this.textBox4);
+			this.groupBox1.Controls.Add(this.label5);
+			this.groupBox1.Controls.Add(this.textBox3);
+			this.groupBox1.Controls.Add(this.label4);
+			this.groupBox1.Controls.Add(this.textBox2);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.textBox1);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.mothername1);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Location = new System.Drawing.Point(32, 21);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(327, 392);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Mother and Children Name";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(218, 276);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 24;
+			this.button1.Text = "Add";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click_1);
+			// 
+			// age6
+			// 
+			this.age6.Location = new System.Drawing.Point(250, 194);
+			this.age6.Name = "age6";
+			this.age6.Size = new System.Drawing.Size(70, 20);
+			this.age6.TabIndex = 23;
+			// 
+			// age5
+			// 
+			this.age5.Location = new System.Drawing.Point(250, 161);
+			this.age5.Name = "age5";
+			this.age5.Size = new System.Drawing.Size(70, 20);
+			this.age5.TabIndex = 22;
+			// 
+			// age4
+			// 
+			this.age4.Location = new System.Drawing.Point(250, 124);
+			this.age4.Name = "age4";
+			this.age4.Size = new System.Drawing.Size(70, 20);
+			this.age4.TabIndex = 21;
+			// 
+			// age3
+			// 
+			this.age3.Location = new System.Drawing.Point(250, 98);
+			this.age3.Name = "age3";
+			this.age3.Size = new System.Drawing.Size(70, 20);
+			this.age3.TabIndex = 20;
+			// 
+			// age2
+			// 
+			this.age2.Location = new System.Drawing.Point(250, 61);
+			this.age2.Name = "age2";
+			this.age2.Size = new System.Drawing.Size(70, 20);
+			this.age2.TabIndex = 19;
+			// 
+			// label12
+			// 
+			this.label12.AutoSize = true;
+			this.label12.Location = new System.Drawing.Point(215, 197);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(29, 13);
+			this.label12.TabIndex = 17;
+			this.label12.Text = "Age:";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(215, 164);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(29, 13);
+			this.label11.TabIndex = 16;
+			this.label11.Text = "Age:";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(215, 128);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(29, 13);
+			this.label10.TabIndex = 15;
+			this.label10.Text = "Age:";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(215, 101);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(29, 13);
+			this.label9.TabIndex = 14;
+			this.label9.Text = "Age:";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(215, 65);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(29, 13);
+			this.label8.TabIndex = 13;
+			this.label8.Text = "Age:";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(99, 190);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.TabIndex = 11;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(6, 193);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(48, 13);
+			this.label6.TabIndex = 10;
+			this.label6.Text = "5th Child";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(99, 161);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 9;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(6, 164);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(48, 13);
+			this.label5.TabIndex = 8;
+			this.label5.Text = "4th Child";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(99, 128);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 7;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(6, 131);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(48, 13);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "3rd Child";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(99, 94);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 5;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(6, 97);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(51, 13);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "2nd Child";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(99, 65);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 68);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(47, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "1st Child";
+			// 
+			// mothername1
+			// 
+			this.mothername1.Location = new System.Drawing.Point(99, 35);
+			this.mothername1.Name = "mothername1";
+			this.mothername1.Size = new System.Drawing.Size(100, 20);
+			this.mothername1.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(6, 38);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(78, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Mother\'s Name";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.search);
+			this.groupBox2.Controls.Add(this.mothername2);
+			this.groupBox2.Controls.Add(this.label13);
+			this.groupBox2.Location = new System.Drawing.Point(478, 61);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(200, 141);
+			this.groupBox2.TabIndex = 1;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Search for Mother";
+			// 
+			// search
+			// 
+			this.search.Location = new System.Drawing.Point(86, 78);
+			this.search.Name = "search";
+			this.search.Size = new System.Drawing.Size(75, 23);
+			this.search.TabIndex = 25;
+			this.search.Text = "Search";
+			this.search.UseVisualStyleBackColor = true;
+			this.search.Click += new System.EventHandler(this.search_Click_1);
+			// 
+			// mothername2
+			// 
+			this.mothername2.Location = new System.Drawing.Point(99, 25);
+			this.mothername2.Name = "mothername2";
+			this.mothername2.Size = new System.Drawing.Size(100, 20);
+			this.mothername2.TabIndex = 3;
+			// 
+			// label13
+			// 
+			this.label13.AutoSize = true;
+			this.label13.Location = new System.Drawing.Point(6, 28);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(78, 13);
+			this.label13.TabIndex = 2;
+			this.label13.Text = "Mother\'s Name";
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(480, 218);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(159, 199);
+			this.listBox1.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox mothername1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox age6;
+		private System.Windows.Forms.TextBox age5;
+		private System.Windows.Forms.TextBox age4;
+		private System.Windows.Forms.TextBox age3;
+		private System.Windows.Forms.TextBox age2;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Button search;
+		private System.Windows.Forms.TextBox mothername2;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.ListBox listBox1;
+	}
+}
+
---
+++ labsheet4/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labsheet4
+{
+	public partial class Form1 : Form
+	{
+		private string[] mothers = new string[1];
+		private string[][] children = new string[1][];
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		
+		private void AddChildDataToList(List<string> childData, string childName, string childAge)
+		{
+			if (!string.IsNullOrWhiteSpace(childName))
+			{
+				childData.Add(childName);
+				childData.Add(childAge);
+			}
+		}
+
+	
+
+		private void button1_Click_1(object sender, EventArgs e)
+		{
+			mothers[0] = mothername1.Text;
+
+			List<string> childData = new List<string>();
+
+
+			AddChildDataToList(childData, textBox2.Text, age2.Text);
+			AddChildDataToList(childData, textBox3.Text, age3.Text);
+			AddChildDataToList(childData, textBox4.Text, age4.Text);
+			AddChildDataToList(childData, textBox5.Text, age5.Text);
+			AddChildDataToList(childData, textBox5.Text, age6.Text);
+
+			children[0] = childData.ToArray();
+
+			mothername1.Text = "";
+			textBox1.Text = "";
+			age2.Text = "";
+			textBox2.Text = "";
+			age3.Text = "";
+			textBox3.Text = "";
+			age4.Text = "";
+			textBox4.Text = "";
+			age5.Text = "";
+			textBox5.Text = "";
+			age6.Text = "";
+		}
+
+		private void search_Click_1(object sender, EventArgs e)
+		{
+
+			string searchName = mothername2.Text;
+
+
+			int index = Array.IndexOf(mothers, searchName);
+
+			if (index != -1)
+			{
+
+				string[] childData = children[index];
+
+
+				listBox1.Items.Clear();
+
+				for (int i = 0; i < childData.Length; i += 2)
+				{
+					string childName = childData[i];
+					string childAge = childData[i + 1];
+					listBox1.Items.Add($"Child: {childName}, Age: {childAge}");
+				}
+			}
+			else
+			{
+				listBox1.Items.Clear();
+				MessageBox.Show("Mother's name not found.");
+			}
+
+			mothername2.Text = "";
+		}
+	}
+}
