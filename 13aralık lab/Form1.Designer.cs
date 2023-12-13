8e01d7d966071f3316b2be3bbde2ebb6e01f9de4
dogadeniz <dogadeniz08@hotmail.com>
2023-12-13 14:45:04 +0300

    13DecemberLab
    OOP,C#forms, class ,array

---
+++ 13aralık lab/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace _13aralık_lab
+{
+	partial class Form1
+	{
+		/// <summary>
+		///Gerekli tasarımcı değişkeni.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///Kullanılan tüm kaynakları temizleyin.
+		/// </summary>
+		///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer üretilen kod
+
+		/// <summary>
+		/// Tasarımcı desteği için gerekli metot - bu metodun 
+		///içeriğini kod düzenleyici ile değiştirmeyin.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(423, 344);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(96, 46);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Insert";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(165, 40);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(67, 40);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(21, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "ID:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(67, 83);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(38, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Name:";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(165, 80);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 4;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(67, 130);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(52, 13);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Surname:";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(165, 127);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 6;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(57, 177);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(62, 13);
+			this.label4.TabIndex = 9;
+			this.label4.Text = "OOP Grade";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(165, 174);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 8;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(51, 229);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(79, 13);
+			this.label5.TabIndex = 11;
+			this.label5.Text = "Calculus Grade";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(165, 226);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.TabIndex = 10;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(51, 283);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(108, 13);
+			this.label6.TabIndex = 13;
+			this.label6.Text = "Data Structure Grade";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(165, 280);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(100, 20);
+			this.textBox6.TabIndex = 12;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(423, 37);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(162, 237);
+			this.richTextBox1.TabIndex = 14;
+			this.richTextBox1.Text = "";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(146, 344);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(96, 46);
+			this.button2.TabIndex = 15;
+			this.button2.Text = "Save";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
---
+++ 13aralık lab/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13aralık_lab
+{
+	public partial class Form1 : Form
+	{
+		private Student[] students; 
+
+		public Form1()
+		{
+			InitializeComponent();
+			students = new Student[100]; 
+		}
+
+		public class Student
+		{
+			public int IdNumber { get; set; }
+			public string Name { get; set; }
+			public string Surname { get; set; }
+			public int OopGrade { get; set; }
+			public int CalculusGrade { get; set; }
+			public int DataStructureGrade { get; set; }
+			public int StudentAvg { get; private set; }
+
+			public Student(int idNumber, string name, string surname, int oopGrade, int calculusGrade, int dataStructureGrade)
+			{
+				IdNumber = idNumber;
+				Name = name;
+				Surname = surname;
+				OopGrade = oopGrade;
+				CalculusGrade = calculusGrade;
+				DataStructureGrade = dataStructureGrade;
+
+				CalculateAvg();
+			}
+
+			private void CalculateAvg()
+			{
+				StudentAvg = (OopGrade + CalculusGrade + DataStructureGrade) / 3;
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			richTextBox1.Clear();
+
+			foreach (var student in students)
+			{
+				if (student != null)
+				{
+					richTextBox1.AppendText($"Id: {student.IdNumber}, Name: {student.Name}, Surname: {student.Surname}, Avg: {student.StudentAvg}\n");
+				}
+				else
+				{
+					
+					break;
+				}
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			Student newStudent = new Student(Convert.ToInt32(textBox1.Text), textBox2.Text, textBox3.Text, Convert.ToInt32(textBox4.Text), Convert.ToInt32(textBox5.Text), Convert.ToInt32(textBox6.Text));
+
+			for (int i = 0; i < students.Length; i++)
+			{
+				if (students[i] == null)
+				{
+					students[i] = newStudent;
+					break;
+				}
+			}
+
+			
+			textBox1.Clear();
+			textBox2.Clear();
+			textBox3.Clear();
+			textBox4.Clear();
+			textBox5.Clear();
+			textBox6.Clear();
+		}
+
+	}
+}
+
